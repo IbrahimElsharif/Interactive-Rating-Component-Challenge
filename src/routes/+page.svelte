c324a7e84d07b0e38f3863f6da6436de2da66b7b
IbrahimElsharif <Elsharif4458@outlook.com>
2023-10-08 21:56:19 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<main>
+    <h1 class="text-violet-400 text-2xl">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+</main>
